06e1c43255835deae9a856d3ce51732016999e0d
eapzzz <125407845+eapzzz@users.noreply.github.com>
2026-07-23 23:38:11 +0200

    feat: implement keyboard movement mechanics

---
+++ player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+var speed = 600
+
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * speed
+	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		%HappyBoo.play_walk_animation()
+	else:
+		%HappyBoo.play_idle_animation()
+	pass
