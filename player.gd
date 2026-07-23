de3dc8a9e1401a67c2c5dcfb069ac8dd2e3e7b9b
eapzzz <125407845+eapzzz@users.noreply.github.com>
2026-07-23 23:46:23 +0200

    feat: setup game scene

--- player.gd
+++ player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 var speed = 600
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * speed
 	move_and_slide()
