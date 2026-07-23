00046446312e13fb73d263be1a69e651c021428a
eapzzz <125407845+eapzzz@users.noreply.github.com>
2026-07-24 00:24:50 +0200

    feat: create enemy scene and basic ai movement

---
+++ mob.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@onready var player = get_node("/root/Game/Player")
+
+var speed = 300.0
+
+
+func _physics_process(_delta: float) -> void:
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * speed
+	move_and_slide()
+	pass
