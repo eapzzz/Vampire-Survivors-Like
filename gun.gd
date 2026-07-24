5c279e86d2929592527303d1b57a6c9e57e84365
eapzzz <125407845+eapzzz@users.noreply.github.com>
2026-07-24 13:52:50 +0200

    feat: setup proper shooting mechanics

--- gun.gd
+++ gun.gd
@@ -8,3 +8,16 @@ func _physics_process(_delta: float) -> void:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
 	pass
+
+
+func shoot():
+	const BULLET = preload("uid://b5ixw66oaedki")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = $WeaponPivot/Pistol/ShootingPoint.global_position
+	new_bullet.global_rotation = $WeaponPivot/Pistol/ShootingPoint.global_rotation
+	$WeaponPivot/Pistol/ShootingPoint.add_child(new_bullet)
+	pass
+
+func _on_timer_timeout() -> void:
+	shoot()
+	pass
