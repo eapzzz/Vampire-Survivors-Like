0be98bbd3a9149d806aef62011e7fd2df89aa27c
eapzzz <125407845+eapzzz@users.noreply.github.com>
2026-07-24 13:58:20 +0200

    feat: add health and hurt animations on mobs

--- mob.gd
+++ mob.gd
@@ -3,10 +3,28 @@ extends CharacterBody2D
 @onready var player = get_node("/root/Game/Player")
 
 var speed = 300.0
+var health = 3
+
+
+func _ready() -> void:
+	$Slime.play_walk()
+	pass
 
 
 func _physics_process(_delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * speed
 	move_and_slide()
+	pass
+
+
+func take_damage():
+	health -= 1
+	$Slime.play_hurt()
+	if health == 0:
+		queue_free()
+		const SMOKE_EXPLOSION = preload("uid://dhmhmrth6rdce")
+		var smoke = SMOKE_EXPLOSION.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
 	pass
