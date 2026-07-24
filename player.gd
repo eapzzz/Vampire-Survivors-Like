a3b8fec13f2c50b3ebed18d3368b3b9097169930
eapzzz <125407845+eapzzz@users.noreply.github.com>
2026-07-24 14:09:35 +0200

    feat: implement health system to player

--- player.gd
+++ player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
+signal health_depleted
+
 var speed = 600
+var health = 100.0
+var damage_rate = 5.0
 
-
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * speed
 	move_and_slide()
@@ -12,4 +15,11 @@ func _physics_process(_delta: float) -> void:
 		%HappyBoo.play_walk_animation()
 	else:
 		%HappyBoo.play_idle_animation()
+	
+	var overlapping_mobs = $HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= damage_rate * overlapping_mobs.size() * delta
+		$ProgressBar.value = health
+		if health <= 0:
+			health_depleted.emit()
 	pass
